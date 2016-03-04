ce5ba162199c68129e1c71b19fcf121b6b5ce36b
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-03-05 08:48:22 +1300

    Echo query parameter rule implementation.

---
+++ source/corvusoft/restq/detail/rule/echo.hpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2014-2016, Corvusoft Ltd, All Rights Reserved.
+ */
+
+#ifndef _RESTQ_DETAIL_RULE_ECHO_H
+#define _RESTQ_DETAIL_RULE_ECHO_H 1
+
+//System Includes
+#include <map>
+#include <string>
+#include <memory>
+#include <ciso646>
+#include <functional>
+
+//Project Includes
+
+//External Includes
+#include <corvusoft/restbed/rule.hpp>
+#include <corvusoft/restbed/string.hpp>
+#include <corvusoft/restbed/session.hpp>
+#include <corvusoft/restbed/request.hpp>
+
+//System Namespaces
+using std::string;
+using std::function;
+using std::multimap;
+using std::shared_ptr;
+
+//Project Namespaces
+
+//External Namespaces
+using restbed::Rule;
+using restbed::Request;
+using restbed::Session;
+
+namespace restq
+{
+    namespace detail
+    {
+        class Echo final : public Rule
+        {
+            public:
+                Echo( void ) : Rule( )
+                {
+                    return;
+                }
+                
+                virtual ~Echo( void )
+                {
+                    return;
+                }
+                
+                void action( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback ) final override
+                {
+                    const auto request = session->get_request( );
+                    
+                    if ( request->get_method( String::lowercase ) == "head" )
+                    {
+                        session->set( "echo", false );
+                    }
+                    else
+                    {
+                        session->set( "echo", true );
+                        
+                        for ( const auto parameter : request->get_query_parameters( "echo" ) )
+                        {
+                            const auto value = String::lowercase( parameter.second );
+                            session->set( "echo", ( value == "false" or value == "no" or value == "off" or value == "0" ) ? false : true );
+                        }
+                    }
+                    
+                    callback( session );
+                }
+        };
+    }
+}
+
+#endif  /* _RESTQ_DETAIL_RULE_ECHO_H */
