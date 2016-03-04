595a3197e6ab2e51f833ffdc69f9650cf5ec322e
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-03-05 09:15:50 +1300

    Location header rule implementation.

---
+++ source/corvusoft/restq/detail/rule/location.hpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2014-2016, Corvusoft Ltd, All Rights Reserved.
+ */
+
+#ifndef _RESTQ_DETAIL_RULE_LOCATION_H
+#define _RESTQ_DETAIL_RULE_LOCATION_H 1
+
+//System Includes
+#include <map>
+#include <list>
+#include <string>
+#include <memory>
+#include <functional>
+
+//Project Includes
+
+//External Includes
+#include <corvusoft/restbed/rule.hpp>
+#include <corvusoft/restbed/string.hpp>
+#include <corvusoft/restbed/session.hpp>
+#include <corvusoft/restbed/request.hpp>
+
+//System Namespaces
+using std::list;
+using std::string;
+using std::function;
+using std::multimap;
+using std::shared_ptr;
+
+//Project Namespaces
+
+//External Namespaces
+using restbed::Rule;
+using restbed::Session;
+
+namespace restq
+{
+    namespace detail
+    {
+        class Location final : public Rule
+        {
+            public:
+                Location( void ) : Rule( )
+                {
+                    return;
+                }
+                
+                virtual ~Location( void )
+                {
+                    return;
+                }
+                
+                bool condition( const shared_ptr< Session > ) final override
+                {
+                    return false;
+                }
+                
+                void action( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback ) final override
+                {
+                    callback( session );
+                }
+                
+                static string make( const shared_ptr< Session >& session, const list< multimap< string, Bytes > >& resources )
+                {
+                    const string host = session->get( "host" );
+                    const auto type = resources.back( ).lower_bound( "type" )->second;
+                    
+                    auto location = String::format( "http://%s/%.*ss", host.data( ), type.size( ), type.data( ) );
+                    
+                    if ( resources.size( ) == 1 )
+                    {
+                        location += "/" + String::to_string( resources.back( ).lower_bound( "key" )->second );
+                    }
+                    else
+                    {
+                        location += "?keys=";
+                        
+                        for ( const auto resource : resources )
+                        {
+                            location += String::to_string( resource.lower_bound( "key" )->second ) + ",";
+                        }
+                        
+                        if ( location.back( ) == ',' ) //String::trim_lagging( location, ',' );
+                        {
+                            location = location.substr( 0, location.length( ) - 1 );
+                        }
+                    }
+                    
+                    return location;
+                }
+        };
+    }
+}
+
+#endif  /* _RESTQ_DETAIL_RULE_LOCATION_H */
