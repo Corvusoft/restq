4001d6ecfdd33e7bc253c67b552068f26eb9d290
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-03-05 09:15:02 +1300

    Last-Modified header rule implementation.

---
+++ source/corvusoft/restq/detail/rule/last_modified.hpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014-2016, Corvusoft Ltd, All Rights Reserved.
+ */
+
+#ifndef _RESTQ_DETAIL_RULE_LAST_MODIFIED_H
+#define _RESTQ_DETAIL_RULE_LAST_MODIFIED_H 1
+
+//System Includes
+#include <map>
+#include <list>
+#include <string>
+#include <memory>
+#include <functional>
+
+//Project Includes
+#include <corvusoft/restq/string.hpp>
+#include <corvusoft/restq/detail/rule/date.hpp>
+
+//External Includes
+#include <corvusoft/restbed/rule.hpp>
+#include <corvusoft/restbed/session.hpp>
+
+//System Namespaces
+using std::list;
+using std::stoul;
+using std::string;
+using std::function;
+using std::multimap;
+using std::shared_ptr;
+
+//Project Namespaces
+
+//External Namespaces
+using restbed::Rule;
+using restbed::Session;
+
+namespace restq
+{
+    namespace detail
+    {
+        class LastModified final : public Rule
+        {
+            public:
+                LastModified( void ) : Rule( )
+                {
+                    return;
+                }
+                
+                virtual ~LastModified( void )
+                {
+                    return;
+                }
+                
+                void action( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback ) final override
+                {
+                    callback( session );
+                }
+                
+                static string make( void )
+                {
+                    return Date::make( );
+                }
+                
+                static string make( const list< multimap< string, Bytes > >& resources )
+                {
+                    const auto datestamp = String::to_string( resources.back( ).lower_bound( "modified" )->second );
+                    
+                    return Date::make( stoul( datestamp ) );
+                }
+        };
+    }
+}
+
+#endif  /* _RESTQ_DETAIL_RULE_LAST_MODIFIED_H */
