4c15adaee2fd424538e089e375ca1ec5bcbcdccd
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-03-07 14:00:10 +1300

    Removed namespace from formatter.

--- example/http_exchange/source/json_formatter.cpp
+++ example/http_exchange/source/json_formatter.cpp
@@ -30,251 +30,248 @@ using restq::String;
 using restq::Formatter;
 using nlohmann::json;
 
-namespace restqd
+JSONFormatter::JSONFormatter( void )
 {
-    JSONFormatter::JSONFormatter( void )
+    return;
+}
+
+JSONFormatter::~JSONFormatter( void )
+{
+    return;
+}
+
+list< multimap< string, Bytes > > JSONFormatter::parse( const Bytes& value )
+{
+    auto json = json::parse( String::to_string( value ) );
+    
+    if ( json.count( "data" ) == 0 )
     {
-        return;
+        throw domain_error( "Root property must be named 'data'." );
     }
     
-    JSONFormatter::~JSONFormatter( void )
-    {
-        return;
-    }
+    auto data = json[ "data" ];
+    list< multimap< string, Bytes > > resources;
     
-    list< multimap< string, Bytes > > JSONFormatter::parse( const Bytes& value )
+    if ( data.is_array( ) )
     {
-        auto json = json::parse( String::to_string( value ) );
-        
-        if ( json.count( "data" ) == 0 )
+        for ( const auto object : data )
         {
-            throw domain_error( "Root property must be named 'data'." );
-        }
-        
-        auto data = json[ "data" ];
-        list< multimap< string, Bytes > > resources;
-        
-        if ( data.is_array( ) )
-        {
-            for ( const auto object : data )
+            if ( not object.is_object( ) )
             {
-                if ( not object.is_object( ) )
-                {
-                    throw domain_error( "Root array child elements must be objects." );
-                }
-                
-                resources.push_back( parse_object( object ) );
-            }
-        }
-        else if ( data.is_object( ) )
-        {
-            resources.push_back( parse_object( data ) );
-        }
-        else
-        {
-            throw domain_error( "Root property must be unordered set or object." );
-        }
-        
-        return resources;
-    }
-    
-    bool JSONFormatter::try_parse( const Bytes& value, list< multimap< string, Bytes > >& values ) noexcept
-    {
-        try
-        {
-            values = parse( value );
-        }
-        catch ( const exception& )
-        {
-            return false;
-        }
-        
-        return true;
-    }
-    
-    Bytes JSONFormatter::compose( const list< multimap< string, Bytes > >& values, const bool styled )
-    {
-        auto json = json::object( );
-        
-        if ( values.size( ) == 1 )
-        {
-            json[ "data" ] = compose_object( values.front( ) );
-        }
-        else
-        {
-            auto objects = json::array( );
-            
-            for ( const auto& value : values )
-            {
-                objects.push_back( compose_object( value ) );
+                throw domain_error( "Root array child elements must be objects." );
             }
             
-            json[ "data" ] = objects;
+            resources.push_back( parse_object( object ) );
+        }
+    }
+    else if ( data.is_object( ) )
+    {
+        resources.push_back( parse_object( data ) );
+    }
+    else
+    {
+        throw domain_error( "Root property must be unordered set or object." );
+    }
+    
+    return resources;
+}
+
+bool JSONFormatter::try_parse( const Bytes& value, list< multimap< string, Bytes > >& values ) noexcept
+{
+    try
+    {
+        values = parse( value );
+    }
+    catch ( const exception& )
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+Bytes JSONFormatter::compose( const list< multimap< string, Bytes > >& values, const bool styled )
+{
+    auto json = json::object( );
+    
+    if ( values.size( ) == 1 )
+    {
+        json[ "data" ] = compose_object( values.front( ) );
+    }
+    else
+    {
+        auto objects = json::array( );
+        
+        for ( const auto& value : values )
+        {
+            objects.push_back( compose_object( value ) );
         }
         
-        return to_bytes( json, styled );
+        json[ "data" ] = objects;
     }
     
-    const string JSONFormatter::get_mime_type( void ) const
+    return to_bytes( json, styled );
+}
+
+const string JSONFormatter::get_mime_type( void ) const
+{
+    return "application/json";
+}
+
+string JSONFormatter::to_string( const json& value ) const
+{
+    if ( value.is_string( ) )
     {
-        return "application/json";
+        return value.get< string >( );
+    }
+    else if ( value.is_boolean( ) )
+    {
+        return ( value.get< bool >( ) == true ) ? "true" : "false";
+    }
+    else if ( value.is_number_integer( ) )
+    {
+        return ::to_string( value.get< long >( ) );
+    }
+    else if ( value.is_number_float( ) )
+    {
+        return ::to_string( value.get< double >( ) );
     }
     
-    string JSONFormatter::to_string( const json& value ) const
+    throw domain_error( "Data-type not supported." );
+}
+
+Bytes JSONFormatter::to_bytes( const json& json, bool styled ) const
+{
+    auto data = ( styled ) ? json.dump( 4 ) : json.dump( );
+    return Bytes( data.begin( ), data.end( ) );
+}
+
+json JSONFormatter::compose_object( const multimap< string, Bytes >& value ) const
+{
+    auto object = json::object( );
+    
+    for ( auto iterator = value.begin( ); iterator not_eq value.end( ); iterator++ )
     {
-        if ( value.is_string( ) )
-        {
-            return value.get< string >( );
-        }
-        else if ( value.is_boolean( ) )
-        {
-            return ( value.get< bool >( ) == true ) ? "true" : "false";
-        }
-        else if ( value.is_number_integer( ) )
-        {
-            return ::to_string( value.get< long >( ) );
-        }
-        else if ( value.is_number_float( ) )
-        {
-            return ::to_string( value.get< double >( ) );
-        }
+        auto iterators = value.equal_range( iterator->first );
+        const auto length = distance( iterators.first, iterators.second );
         
-        throw domain_error( "Data-type not supported." );
-    }
-    
-    Bytes JSONFormatter::to_bytes( const json& json, bool styled ) const
-    {
-        auto data = ( styled ) ? json.dump( 4 ) : json.dump( );
-        return Bytes( data.begin( ), data.end( ) );
-    }
-    
-    json JSONFormatter::compose_object( const multimap< string, Bytes >& value ) const
-    {
-        auto object = json::object( );
-        
-        for ( auto iterator = value.begin( ); iterator not_eq value.end( ); iterator++ )
+        if ( length > 1 )
         {
-            auto iterators = value.equal_range( iterator->first );
-            const auto length = distance( iterators.first, iterators.second );
+            auto items = json::array( );
             
-            if ( length > 1 )
+            for ( auto item = iterators.first; item not_eq iterators.second; item++ )
             {
-                auto items = json::array( );
-                
-                for ( auto item = iterators.first; item not_eq iterators.second; item++ )
-                {
-                    auto field = String::to_string( item->second );
-                    
-                    if ( String::is_boolean( field ) )
-                    {
-                        items.push_back( String::lowercase( field ) == "true" );
-                    }
-                    else if ( String::is_integer( field ) )
-                    {
-                        try
-                        {
-                            items.push_back( stol( field ) );
-                        }
-                        catch ( const out_of_range& )
-                        {
-                            items.push_back( field );
-                        }
-                    }
-                    else if ( String::is_fraction( field ) )
-                    {
-                        try
-                        {
-                            items.push_back( stod( field ) );
-                        }
-                        catch ( const out_of_range& )
-                        {
-                            items.push_back( field );
-                        }
-                    }
-                    else
-                    {
-                        items.push_back( field );
-                    }
-                }
-                
-                object[ iterator->first ] = items;
-                
-                advance( iterator, length - 1 );
-            }
-            else
-            {
-                auto field = String::to_string( iterator->second );
+                auto field = String::to_string( item->second );
                 
                 if ( String::is_boolean( field ) )
                 {
-                    object[ iterator->first ] = ( String::lowercase( field ) == "true" );
+                    items.push_back( String::lowercase( field ) == "true" );
                 }
                 else if ( String::is_integer( field ) )
                 {
                     try
                     {
-                        object[ iterator->first ] = stol( field );
+                        items.push_back( stol( field ) );
                     }
                     catch ( const out_of_range& )
                     {
-                        object[ iterator->first ] = field;
+                        items.push_back( field );
                     }
                 }
                 else if ( String::is_fraction( field ) )
                 {
                     try
                     {
-                        object[ iterator->first ] = stod( field );
+                        items.push_back( stod( field ) );
                     }
                     catch ( const out_of_range& )
                     {
-                        object[ iterator->first ] = field;
+                        items.push_back( field );
                     }
                 }
                 else
                 {
+                    items.push_back( field );
+                }
+            }
+            
+            object[ iterator->first ] = items;
+            
+            advance( iterator, length - 1 );
+        }
+        else
+        {
+            auto field = String::to_string( iterator->second );
+            
+            if ( String::is_boolean( field ) )
+            {
+                object[ iterator->first ] = ( String::lowercase( field ) == "true" );
+            }
+            else if ( String::is_integer( field ) )
+            {
+                try
+                {
+                    object[ iterator->first ] = stol( field );
+                }
+                catch ( const out_of_range& )
+                {
                     object[ iterator->first ] = field;
                 }
             }
-        }
-        
-        return object;
-    }
-    
-    multimap< string, Bytes > JSONFormatter::parse_object( const json& value ) const
-    {
-        multimap< string, Bytes > object;
-        
-        for ( auto iterator = value.begin( ); iterator not_eq value.end( ); iterator++ )
-        {
-            if ( iterator->is_object( ) )
+            else if ( String::is_fraction( field ) )
             {
-                throw domain_error( "Child objects are not supported." );
-            }
-            else if ( iterator->is_array( ) )
-            {
-                for ( const auto& item : *iterator )
+                try
                 {
-                    if ( item.is_object( ) )
-                    {
-                        throw domain_error( "Child objects are not supported." );
-                    }
-                    else if ( item.is_array( ) )
-                    {
-                        throw domain_error( "Child unordered sets are not supported." );
-                    }
-                    
-                    auto value = to_string( item );
-                    object.insert( make_pair( iterator.key( ), String::to_bytes( value ) ) );
+                    object[ iterator->first ] = stod( field );
+                }
+                catch ( const out_of_range& )
+                {
+                    object[ iterator->first ] = field;
                 }
             }
             else
             {
-                auto value = to_string( *iterator );
+                object[ iterator->first ] = field;
+            }
+        }
+    }
+    
+    return object;
+}
+
+multimap< string, Bytes > JSONFormatter::parse_object( const json& value ) const
+{
+    multimap< string, Bytes > object;
+    
+    for ( auto iterator = value.begin( ); iterator not_eq value.end( ); iterator++ )
+    {
+        if ( iterator->is_object( ) )
+        {
+            throw domain_error( "Child objects are not supported." );
+        }
+        else if ( iterator->is_array( ) )
+        {
+            for ( const auto& item : *iterator )
+            {
+                if ( item.is_object( ) )
+                {
+                    throw domain_error( "Child objects are not supported." );
+                }
+                else if ( item.is_array( ) )
+                {
+                    throw domain_error( "Child unordered sets are not supported." );
+                }
+                
+                auto value = to_string( item );
                 object.insert( make_pair( iterator.key( ), String::to_bytes( value ) ) );
             }
         }
-        
-        return object;
+        else
+        {
+            auto value = to_string( *iterator );
+            object.insert( make_pair( iterator.key( ), String::to_bytes( value ) ) );
+        }
     }
+    
+    return object;
 }
--- example/http_exchange/source/json_formatter.hpp
+++ example/http_exchange/source/json_formatter.hpp
@@ -2,8 +2,8 @@
  * Copyright 2013-2016, Corvusoft Ltd, All Rights Reserved.
  */
 
-#ifndef _RESTQD_JSON_FORMATTER_H
-#define _RESTQD_JSON_FORMATTER_H 1
+#ifndef _JSON_FORMATTER_H
+#define _JSON_FORMATTER_H 1
 
 //System Includes
 #include <map>
@@ -24,81 +24,78 @@
 
 //External Namespaces
 
-namespace restqd
-{
-    //Forward Declarations
-    
-    class JSONFormatter final : public restq::Formatter
-    {
-        public:
-            //Friends
-            
-            //Definitions
-            
-            //Constructors
-            JSONFormatter( void );
-            
-            virtual ~JSONFormatter( void );
-            
-            //Functionality
-            std::list< std::multimap< std::string, restq::Bytes > > parse( const restq::Bytes& value );
-            
-            bool try_parse( const restq::Bytes& value, std::list< std::multimap< std::string, restq::Bytes > >& values ) noexcept;
-            
-            restq::Bytes compose( const std::list< std::multimap< std::string, restq::Bytes > >& values, const bool styled = false );
-            
-            //Getters
-            const std::string get_mime_type( void ) const;
-            
-            //Setters
-            
-            //Operators
-            
-            //Properties
-            
-        protected:
-            //Friends
-            
-            //Definitions
-            
-            //Constructors
-            
-            //Functionality
-            
-            //Getters
-            
-            //Setters
-            
-            //Operators
-            
-            //Properties
-            
-        private:
-            //Friends
-            
-            //Definitions
-            
-            //Constructors
-            JSONFormatter( const JSONFormatter& original ) = delete;
-            
-            //Functionality
-            std::string to_string( const nlohmann::json& value ) const;
-            
-            restq::Bytes to_bytes( const nlohmann::json& json, bool styled ) const;
-            
-            nlohmann::json compose_object( const std::multimap< std::string, restq::Bytes >& value ) const;
-            
-            std::multimap< std::string, restq::Bytes > parse_object( const nlohmann::json& value ) const;
-            
-            //Getters
-            
-            //Setters
-            
-            //Operators
-            JSONFormatter& operator =( const JSONFormatter& value ) = delete;
-            
-            //Properties
-    };
-}
+//Forward Declarations
 
-#endif  /* _RESTQD_JSON_FORMATTER_H */
+class JSONFormatter final : public restq::Formatter
+{
+    public:
+        //Friends
+        
+        //Definitions
+        
+        //Constructors
+        JSONFormatter( void );
+        
+        virtual ~JSONFormatter( void );
+        
+        //Functionality
+        std::list< std::multimap< std::string, restq::Bytes > > parse( const restq::Bytes& value );
+        
+        bool try_parse( const restq::Bytes& value, std::list< std::multimap< std::string, restq::Bytes > >& values ) noexcept;
+        
+        restq::Bytes compose( const std::list< std::multimap< std::string, restq::Bytes > >& values, const bool styled = false );
+        
+        //Getters
+        const std::string get_mime_type( void ) const;
+        
+        //Setters
+        
+        //Operators
+        
+        //Properties
+        
+    protected:
+        //Friends
+        
+        //Definitions
+        
+        //Constructors
+        
+        //Functionality
+        
+        //Getters
+        
+        //Setters
+        
+        //Operators
+        
+        //Properties
+        
+    private:
+        //Friends
+        
+        //Definitions
+        
+        //Constructors
+        JSONFormatter( const JSONFormatter& original ) = delete;
+        
+        //Functionality
+        std::string to_string( const nlohmann::json& value ) const;
+        
+        restq::Bytes to_bytes( const nlohmann::json& json, bool styled ) const;
+        
+        nlohmann::json compose_object( const std::multimap< std::string, restq::Bytes >& value ) const;
+        
+        std::multimap< std::string, restq::Bytes > parse_object( const nlohmann::json& value ) const;
+        
+        //Getters
+        
+        //Setters
+        
+        //Operators
+        JSONFormatter& operator =( const JSONFormatter& value ) = delete;
+        
+        //Properties
+};
+
+#endif  /* _JSON_FORMATTER_H */
